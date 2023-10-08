a0f777575cc3b89e42a76b7f85e40f00a84be3d8
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-10-07 21:24:08 -0300

    coments

--- src/tp12basededatos/TP12BaseDeDatos.java
+++ src/tp12basededatos/TP12BaseDeDatos.java
@@ -71,7 +71,7 @@ public class TP12BaseDeDatos {
                JOptionPane.showMessageDialog(null, "Error SQL");
            }
         }*/
-        try {
+        /*try {
                 // Cargar el Driver de conexión
             Class.forName("org.mariadb.jdbc.Driver");
             // Establecer la conexión a la base de datos
@@ -112,7 +112,7 @@ public class TP12BaseDeDatos {
            } else{
                JOptionPane.showMessageDialog(null, "Error SQL");
            }
-        }
+        }*/
                /*try {
                 // Cargar el Driver de conexión
             Class.forName("org.mariadb.jdbc.Driver");
